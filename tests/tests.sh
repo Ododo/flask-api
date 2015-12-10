69851ce04bd931e404c756e55f63babdf8e9a7a9
Olivier <olivierdautricourt@gmail.com>
2015-12-10 18:45:30 +0100

    fix curl syntaxe

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo -e "\n\n***User list"
-curl localhost:5000/user/
+curl 127.0.0.1:5000/user
 echo -e "\n\n*** User 1 (not exists)"
 curl localhost:5000/user/1
 echo -e "\n\n***Create user"
-curl -H "Content-Type: application/json" -X POST -d '{"email":"test@test","username":"test","password":"1234"}' localhost:5000/user/
+curl -H "Content-Type: application/json" -X POST -d '{"email":"test@test","username":"test","password":"1234"}' localhost:5000/user
 echo -e "\n\n***User 1 (not exists)"
 curl localhost:5000/user/1
 echo -e "\n\n***User 2 (exists)"
 curl localhost:5000/user/2
 echo -e "\n\n***Exercise list (global)"
-curl localhost:5000/exercise/
+curl localhost:5000/exercise
 echo -e "\n\n***Get user 1 exercise list (none)"
-curl localhost:5000/user/1/exercise/
+curl localhost:5000/user/1/exercise
 echo -e "\n\n***Get User 2 exercise list (exists)"
-curl localhost:5000/user/2/exercise/
+curl localhost:5000/user/2/exercise
 echo -e "\n\n***User 2 exercice 1 (exists)"
 curl localhost:5000/user/2/exercise/1
 echo -e "\n\n***User 2 exercice 8 (not exists)"
--- tests/tokens.sh
+++ tests/tokens.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo -e "\n\n*** User List"
-curl localhost:5000/user/
+curl localhost:5000/user
 
 echo -e "\n\n*** Get token for user \"test\" with wrong password"
-curl -H "Content-Type: application/json" -X POST -d '{"username":"test","password":"test"}' localhost:5000/token/
+curl -H "Content-Type: application/json" -X POST -d '{"username":"test","password":"test"}' localhost:5000/token
 
 echo -e "\n\n*** Get token for user \"test\""
-curl -H "Content-Type: application/json" -X POST -d '{"username":"test","password":"1234"}' localhost:5000/token/
+curl -H "Content-Type: application/json" -X POST -d '{"username":"test","password":"1234"}' localhost:5000/token
 
 
 echo -e "\n\n***END TESTS"
